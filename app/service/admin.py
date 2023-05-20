579dedcee7763e7fe0ae13e1209bb4eabd425d38
oyekamal <autocratickamal1@gmail.com>
2023-05-20 10:34:23 +0500

    models added

--- app/service/admin.py
+++ app/service/admin.py
@@ -1,3 +1,89 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserProfile, ServiceArea, ServiceType, ServiceRequest, ServiceProvider, PreviousWork, Offer, ChatMessage
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'full_name',
+        'address',
+        'phone_number',
+        'is_service_provider',
+        'is_client',
+    )
+    list_filter = ('user', 'is_service_provider', 'is_client')
+
+
+@admin.register(ServiceArea)
+class ServiceAreaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+    search_fields = ('name',)
+
+
+@admin.register(ServiceType)
+class ServiceTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+    search_fields = ('name',)
+
+
+@admin.register(ServiceRequest)
+class ServiceRequestAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'client',
+        'service_area',
+        'service_type',
+        'description',
+        'created_at',
+        'status',
+    )
+    list_filter = ('client', 'service_area', 'service_type', 'created_at')
+    date_hierarchy = 'created_at'
+
+
+@admin.register(ServiceProvider)
+class ServiceProviderAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'business_name',
+        'contact_details',
+        'company_logo',
+        'company_description',
+    )
+    list_filter = ('user',)
+
+
+@admin.register(PreviousWork)
+class PreviousWorkAdmin(admin.ModelAdmin):
+    list_display = ('id', 'service_provider', 'image', 'description')
+    list_filter = ('service_provider',)
+
+
+@admin.register(Offer)
+class OfferAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'service_provider',
+        'service_request',
+        'cost_estimate',
+        'completion_time',
+        'status',
+    )
+    list_filter = ('service_provider', 'service_request')
+
+
+@admin.register(ChatMessage)
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'sender',
+        'receiver',
+        'service_request',
+        'message',
+        'timestamp',
+    )
+    list_filter = ('sender', 'receiver', 'service_request', 'timestamp')
--- app/service/models.py
+++ app/service/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    is_service_provider = models.BooleanField(default=False)
+    is_client = models.BooleanField(default=False)
+
+class ServiceArea(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+class ServiceType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+class ServiceRequest(models.Model):
+    client = models.ForeignKey(User, on_delete=models.CASCADE)
+    service_area = models.ForeignKey(ServiceArea, on_delete=models.CASCADE)
+    service_type = models.ForeignKey(ServiceType, on_delete=models.CASCADE)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=[("Open", "Open"), ("In Progress", "In Progress"), ("Completed", "Completed")])
+
+class ServiceProvider(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    business_name = models.CharField(max_length=255)
+    contact_details = models.CharField(max_length=255)
+    company_logo = models.ImageField(upload_to="company_logos/")
+    company_description = models.TextField()
+
+class PreviousWork(models.Model):
+    service_provider = models.ForeignKey(ServiceProvider, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="previous_work/")
+    description = models.TextField()
+
+class Offer(models.Model):
+    service_provider = models.ForeignKey(ServiceProvider, on_delete=models.CASCADE)
+    service_request = models.ForeignKey(ServiceRequest, on_delete=models.CASCADE)
+    cost_estimate = models.DecimalField(max_digits=10, decimal_places=2)
+    completion_time = models.PositiveIntegerField()
+    status = models.CharField(max_length=20, choices=[("Pending", "Pending"), ("Accepted", "Accepted"), ("Rejected", "Rejected")])
+
+class ChatMessage(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender_messages')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver_messages')
+    service_request = models.ForeignKey(ServiceRequest, on_delete=models.CASCADE)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    
