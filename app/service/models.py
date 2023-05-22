7a27e5d88488120ad535712087a1009857f8b664
oyekamal <autocratickamal1@gmail.com>
2023-05-22 22:00:01 +0500

    model updated

--- app/service/models.py
+++ app/service/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User
 
 
-
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=255)
@@ -12,13 +11,25 @@ class UserProfile(models.Model):
     is_service_provider = models.BooleanField(default=False)
     is_client = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.full_name
+
+
 class ServiceArea(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
+
 class ServiceType(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
 
 class ServiceRequest(models.Model):
     client = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
@@ -26,7 +37,12 @@ class ServiceRequest(models.Model):
     service_type = models.ForeignKey(ServiceType, on_delete=models.CASCADE)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=20, choices=[("Open", "Open"), ("In Progress", "In Progress"), ("Completed", "Completed")])
+    status = models.CharField(max_length=20, choices=[(
+        "Open", "Open"), ("In Progress", "In Progress"), ("Completed", "Completed")])
+
+    def __str__(self):
+        return self.description[:20]
+
 
 class ServiceProvider(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -35,23 +51,43 @@ class ServiceProvider(models.Model):
     company_logo = models.ImageField(upload_to="company_logos/")
     company_description = models.TextField()
 
+    def __str__(self):
+        return self.business_name
+
+
 class PreviousWork(models.Model):
-    service_provider = models.ForeignKey(ServiceProvider, on_delete=models.CASCADE)
+    service_provider = models.ForeignKey(
+        ServiceProvider, on_delete=models.CASCADE)
     image = models.ImageField(upload_to="previous_work/")
     description = models.TextField()
 
+    def __str__(self):
+        return self.service_provider.business_name + ": work"
+
+
 class Offer(models.Model):
-    service_provider = models.ForeignKey(ServiceProvider, on_delete=models.CASCADE)
-    service_request = models.ForeignKey(ServiceRequest, on_delete=models.CASCADE)
+    service_provider = models.ForeignKey(
+        ServiceProvider, on_delete=models.CASCADE)
+    service_request = models.ForeignKey(
+        ServiceRequest, on_delete=models.CASCADE)
     cost_estimate = models.DecimalField(max_digits=10, decimal_places=2)
     completion_time = models.PositiveIntegerField()
-    status = models.CharField(max_length=20, choices=[("Pending", "Pending"), ("Accepted", "Accepted"), ("Rejected", "Rejected")])
+    status = models.CharField(max_length=20, choices=[(
+        "Pending", "Pending"), ("Accepted", "Accepted"), ("Rejected", "Rejected")])
+
+    def __str__(self):
+        return self.service_provider.business_name + ": offer : " + str(self.cost_estimate)
+
 
 class ChatMessage(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender_messages')
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver_messages')
-    service_request = models.ForeignKey(ServiceRequest, on_delete=models.CASCADE)
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='sender_messages')
+    receiver = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='receiver_messages')
+    service_request = models.ForeignKey(
+        ServiceRequest, on_delete=models.CASCADE)
     message = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    
-    
+
+    def __str__(self):
+        return self.sender.username + ": <-> : " + self.receiver.username
